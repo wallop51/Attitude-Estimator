34bab9d6b4d46542585c75b40a2585b1c8cfde09
wallop51 <99040214+wallop51@users.noreply.github.com>
2026-08-14 17:18:40 +0100

    enable EXTI1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -293,6 +293,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
