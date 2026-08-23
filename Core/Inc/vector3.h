5435f0daccb7a7ef75979e888fb60a03ebc12ddc
wallop51 <99040214+wallop51@users.noreply.github.com>
2026-08-24 00:43:35 +0100

    Attitude Algorithm Port to C

--- Core/Inc/vector3.h
+++ Core/Inc/vector3.h
@@ -1,11 +1,19 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
+#include <stdint.h>
+
 typedef struct {
     float x;
     float y;
     float z;
 } Vector3;
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} Vector3_i;
 
 float vector3_magnitude(Vector3 v);
 Vector3 vector3_normalise(Vector3 v);
